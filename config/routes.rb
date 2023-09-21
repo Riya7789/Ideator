13967d2a3d94ba86e2be24520e17c32542f3cfcd
riya <cresthariya7@gmail.com>
2023-09-21 12:59:38 +0545

    Ideator

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "ideas#index"
   resources :ideas
-  
+  get 'about' => 'static_pages#about'
+  get 'random' => 'static_pages#random'
 end
